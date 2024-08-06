24758de7499c59c17ce70f005c2c8e9eb0e8d0de
Olivier3456 <olivier.larrey@gmail.com>
2024-08-06 08:29:45 +0200

    Addded destruction effect to the crate

--- Assets/Scripts/DestructibleCrate.cs
+++ Assets/Scripts/DestructibleCrate.cs
@@ -7,6 +7,8 @@ public class DestructibleCrate : MonoBehaviour
 {
     public static event EventHandler OnAnyDestroyed;
 
+    [SerializeField] private Transform crateDestroyedPrefab;
+
     private GridPosition gridPosition;
 
     private void Start()
@@ -16,6 +18,8 @@ public class DestructibleCrate : MonoBehaviour
 
     public void Damage()
     {
+        Transform crateDestroyedTransform = Instantiate(crateDestroyedPrefab, transform.position, transform.rotation);
+        ApplyExplosionToChildren(crateDestroyedTransform, 150f, transform.position, 10f);
         Destroy(gameObject);
         OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
     }
@@ -23,5 +27,19 @@ public class DestructibleCrate : MonoBehaviour
     public GridPosition GetGridPosition()
     {
         return gridPosition;
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                Vector3 randomDir = new Vector3(UnityEngine.Random.Range(-1f, 1f), 0f, UnityEngine.Random.Range(-1f, 1f));
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition + randomDir, explosionRange);
+            }
+
+            ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRange);
+        }
     }
 }
